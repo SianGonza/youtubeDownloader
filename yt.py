50f59c5c447c7f091417ca5cf61c680619d57ab4
Andres Sian <sianandres97@gmail.com>
2024-02-12 17:39:44 -0300

    Agrego doc

--- yt.py
+++ yt.py
@@ -1,18 +1,18 @@
-from pytube import YouTube
 from sys import argv
 
-# Get the YouTube link from the command line arguments
+from pytube import YouTube
+
+# Para pasar el argumento [LINK DE YOUTUBE]
 link = argv[1]
 
-# Create a YouTube object
 yt = YouTube(link)
 
-# Print video title and views
-print('Title: ', yt.title)
-print('Views: ', yt.views)
+# Imprime  el titulo y visitas
+print("Title: ", yt.title)
+print("Views: ", yt.views)
 
-# Get the highest resolution stream
+# Obtiene el video con la resolucion mas alta y lo descarga en la carpeta descargas
 stream = yt.streams.get_highest_resolution()
 
-# Download the stream to the specified directory
+# Reemplazar directorio por el elegido
 stream.download("C:/Users/siana/Downloads")
